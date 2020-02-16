ef36c906cd08484bb946618c909e7c66f38e4da2
Rob <35349383+RobRoyEerkes@users.noreply.github.com>
2020-02-16 18:50:04 +0100

    Main Animation start
    alleen nog de fight animation

--- Dangerous Dungeons/Assets/PlayerMovement.cs
+++ Dangerous Dungeons/Assets/PlayerMovement.cs
@@ -10,6 +10,8 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody2D rb;
     private Vector3 change;
     public int Dir;
+    private Animator animator;
+    private SpriteRenderer Sprite;
 
     //Camera vars
     public Camera Cam;
@@ -19,6 +21,8 @@ public class PlayerMovement : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        Sprite = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -28,34 +32,44 @@ public class PlayerMovement : MonoBehaviour
         change = Vector3.zero;
         change.x = Input.GetAxisRaw("Horizontal");
         change.y = Input.GetAxisRaw("Vertical");
+        if (change != Vector3.zero)
+            Move(change);
+        else
+            animator.SetBool("Moving", false);
 
-        Move(change);
         //camera moving with the player
         CamChange = rb.transform.position;
-        CamChange.z = -10;
+        CamChange.z = -8;
         Cam.transform.position = CamChange;
+        animator.SetInteger("Dir", Dir);
 
     }
     
     //funcion to move the player
     private void Move(Vector3 Vec3)
     {
-        if (Vec3 != Vector3.zero)
-        {
-            if (Vec3.x != 0 && Vec3.y != 0)
-                rb.MovePosition(transform.position + Vec3 * speed/Mathf.Sqrt(2) * Time.deltaTime);
-            else
-                rb.MovePosition(transform.position + Vec3 * speed * Time.deltaTime);
-           
-            //setting direction value.
-            if (Vec3.x == 1)
-                Dir = 1;
-            else if (Vec3.x == -1)
-                Dir = 3;
-            else if (Vec3.y == 1)
-                Dir = 0;
-            else
-                Dir = 2;
-        }
+        animator.SetBool("Moving", true);
+     
+     
+        if (Vec3.x != 0 && Vec3.y != 0)
+            rb.MovePosition(transform.position + Vec3 * speed/Mathf.Sqrt(2) * Time.deltaTime);
+        else
+            rb.MovePosition(transform.position + Vec3 * speed * Time.deltaTime);
+       
+        //setting direction value.
+        if (Vec3.x == 1)
+            Dir = 1;
+        else if (Vec3.x == -1)
+            Dir = 3;
+        else if (Vec3.y == 1)
+            Dir = 0;
+        else
+            Dir = 2;
+
+        if (Dir == 3)
+            Sprite.flipX = true;
+        else
+            Sprite.flipX = false;
+     
     }
 }
