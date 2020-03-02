44f764c485c91cfce6b2315680dad58d584a36eb
Rob <35349383+RobRoyEerkes@users.noreply.github.com>
2020-03-02 19:24:03 +0100

    666

--- Dangerous Dungeons/Assets/ScriptsRob/nextscene.cs
+++ Dangerous Dungeons/Assets/ScriptsRob/nextscene.cs
@@ -4,16 +4,20 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class nextscene : MonoBehaviour
 {
+    Canvas can;
     private void Start()
     {
-        this.enabled = false;
+        can = GetComponentInChildren<Canvas>();
+        can.enabled = false;
     }
+
     public void goNextScene()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
+
     public void OnTriggerEnter2D(Collider2D collision)
     {
-        this.
+        can.enabled = true;
     }
 }
