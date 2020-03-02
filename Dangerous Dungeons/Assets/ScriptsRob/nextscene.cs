9873e9ac75c067f81deb1a9f7b2c65cc8b48cf1f
jellew01 <jellew@icloud.com>
2020-03-02 19:19:17 +0100

    script next scene en andere zooi

---
+++ Dangerous Dungeons/Assets/ScriptsRob/nextscene.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class nextscene : MonoBehaviour
+{
+    private void Start()
+    {
+        this.enabled = false;
+    }
+    public void goNextScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        this.
+    }
+}
