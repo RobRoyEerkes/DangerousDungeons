4dea964ccc7d04f8afce75c5a0cf6891bf7a397c
Rob <35349383+RobRoyEerkes@users.noreply.github.com>
2020-03-03 12:51:48 +0100

    Update Kraken.cs

--- Dangerous Dungeons/Assets/ScriptsRob/Kraken.cs
+++ Dangerous Dungeons/Assets/ScriptsRob/Kraken.cs
@@ -3,3 +3,20 @@ using System.Generic;
 using UnityEngine;
 
 public class Kraken : MonoBehavior
+{
+    public Player player;
+    private PlayerMovement PlayerM;
+    
+    void Start()
+    {
+        PlayerM = player.GetComponent<PlayerMovement>();
+    }
+    
+    public void ImpairMovement()
+    {
+        
+        PlayerM.enabled = false;
+    } 
+
+
+}
